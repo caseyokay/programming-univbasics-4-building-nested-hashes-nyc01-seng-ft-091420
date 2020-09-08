050c1266390f224908087d77f908a8975d55a472
Casey B <caseyoksana@gmail.com>
2020-09-08 02:24:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,8 +32,8 @@ def monopoly_with_third_tier
   }
   base_hash[:railroads][rent_in_dollars]<<{one_piece_owned: 25}
   base_hash[:railroads][rent_in_dollars]<<{two_piece_owned: 50}
-  base_hash[:railroads][rent_in_dollars]<<{one_piece_owned: 25}
-  base_hash[:railroads][rent_in_dollars]<<{one_piece_owned: 25}
+  base_hash[:railroads][rent_in_dollars]<<{three_piece_owned: 100}
+  base_hash[:railroads][rent_in_dollars]<<{four_piece_owned: 200}
 end
 
 def monopoly_with_fourth_tier
